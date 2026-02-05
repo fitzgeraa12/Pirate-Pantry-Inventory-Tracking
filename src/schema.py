3ac74b993895aba5de6e9c79ac72fc217af88726
fitzgeraa12 <119554347+fitzgeraa12@users.noreply.github.com>
2026-02-05 14:02:41 -0600

    Delete src/schema.py

--- src/schema.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class FoodType(db.Model):
-    def __init__(self, id: int, brand_id: int) -> None:
-        self.id = id
-        self.brand_id = brand_id
