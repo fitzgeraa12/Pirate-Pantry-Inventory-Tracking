99e133dbf6970a66931363a56ac223c0466b8f74
Camille James <156543104+camille-s-james@users.noreply.github.com>
2026-02-15 23:18:44 +0000

    Can update an item through searching its id

--- databasetest.py
+++ databasetest.py
@@ -2,12 +2,6 @@ import sqlite3 #Allows for interaction with the database
 import random #To generate a random id
 
 
-"""Adding an item to the database. id is randomly generated for now"""
-def add(name, brand, group, quantity, cursor):
-    add_query = f'INSERT INTO Test_table VALUES ("{name}", "{brand}", "{random.randint(0, 1000)}", "{group}", "{quantity}", "None")' #Query for adding a record to the database
-    cursor.execute(add_query) #Executes query to add the item
-
-
 """Returns a list of all items sorted by name"""
 def pull_names(cursor):
     cursor.execute('SELECT DISTINCT name FROM Test_table') #Gets all unique item names 
@@ -32,21 +26,38 @@ def group_search(cursor):
 
 """Prints all of the data in the table"""
 def view(cursor):
-    get_table = 'SELECT * FROM Test_table' #SQL command to select all data from the table
-    cursor.execute(get_table) #.execute runs the specific command
+    print("Updated table:")
+    cursor.execute('SELECT * FROM Test_table') #SQL command to select all data from the table - .execute runs the specific command
     print(cursor.fetchall()) #fetchall fetches everything the sql query returns
+
+def update_table(cursor, quantity, id):
+    cursor.execute(f'SELECT quantity FROM Test_table WHERE id = "{id}"')
+    og_quantity = cursor.fetchall()[0][0]
+    new_quantity = int(og_quantity) + int(quantity)    
+    cursor.execute(f'UPDATE Test_table SET quantity = "{new_quantity}" WHERE id = "{id}"')
+
+"""Adds to an item that already exists in the table"""
+def add_by_id(cursor, id):
+    cursor.execute(f'SELECT * FROM Test_table WHERE id = "{id}"') #Searches for item by id
+    get_item = cursor.fetchall()
+    print(f'Adding to "{3}" items to: "{get_item}"') #Double check to make sure the user knows what items they're adding and to what
+    update_table(cursor, 3, id)
+    cursor.execute(f'SELECT * FROM Test_table WHERE id = "{id}"') #Searches the item to get the updated value
+    get_item = cursor.fetchall()
+    print(f'Updated value: {get_item}')
+
+"""Adding an item to the database. id is randomly generated for now"""
+def add_new(name, brand, group, quantity, cursor):
+    cursor.execute(f'INSERT INTO Test_table VALUES ("{name}", "{brand}", "{random.randint(0, 1000)}", "{group}", "{quantity}", "None")') #Query for adding a record to the database
 
 def main():
     connection = sqlite3.connect('/workspaces/Pirate-Pantry-Inventory-Tracking/Test_table.db') #Connects to existing database
     cursor = connection.cursor() #The cursor is what lets you traverse over the database- what allows you to query records!
-    #add('test','Test_table','test3',6, cursor)
+    add_new('Nacho Cheese Doritos','Doritos','Chips',6, cursor) #Manually adds bag of doritos to table
+    add_by_id(cursor, 1222) #Adds 3 more cups of kraft mac n cheese
     view(cursor)
-    #pull_names(cursor)
-    group_search(cursor)
-    #print("Save changes? (y/n)") #User can choose to save data to the table
-    #answer = input()
-    #if answer == "y":
-    #    connection.commit() #Saves all changes to test database
+    pull_names(cursor)
+    #group_search(cursor)
     cursor.close() #Closes connection and cursor
     connection.close()
 
