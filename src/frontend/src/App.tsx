ddd05cd2e1246cafa51adc169ae0dd4a8b9a8bed
Aidan Fitzgerld <fitzgeraa@southwestern.edu>
2026-02-15 14:55:33 -0600

    Basic google auth

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import AddItem from './inventory/AddItem';
+import { GoogleLogin, type CredentialResponse } from '@react-oauth/google';
 
 function App() {
+    const [auth_token, set_auth_token] = useState<string | null>(null);
     const [add_item_enabled, set_add_item_enabled] = useState(true);
 
+    // https://www.npmjs.com/package/@react-oauth/google?activeTab=readme
+    // https://blog.logrocket.com/guide-adding-google-login-react-app/
+    const google_res = (res: CredentialResponse) => {
+        set_auth_token(res.credential!);
+    };
+    const google_err = () => {
+        console.error("Login failed");
+    };
+
+    if (!auth_token) {
+        return <>
+            <GoogleLogin onSuccess={google_res} onError={google_err}></GoogleLogin>
+        </>
+    }
+    
     return (
         <>
             {add_item_enabled && <AddItem />}
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from 'react';
+
+const CLIENT_ID = "391677624577-24ihed14clpj1d3ioumsq08aeagrj30n.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <StrictMode>
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+        </GoogleOAuthProvider>
+    </StrictMode>
 )
