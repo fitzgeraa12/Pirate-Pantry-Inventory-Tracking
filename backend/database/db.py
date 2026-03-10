05fbfa2dbfb06d965b15ab3cb10be647fb580271
Aidan Fitzgerald <fitzgeraa@southwestern.edu>
2026-03-10 01:56:21 -0500

    tweaking backend code

--- backend/database/db.py
+++ backend/database/db.py
@@ -26,6 +26,7 @@ def query(sql, params=None):
     if params:
         body['params'] = params
 
+    print(f"TOKEN: '{API_TOKEN}'", flush=True)
     response = requests.post(API_URL, headers=headers, json=body)
     response.raise_for_status()
     data = response.json()
