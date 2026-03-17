9e5712699ac1a1223ad847023c1161b3b11b999c
Camille James <156543104+camille-s-james@users.noreply.github.com>
2026-03-17 01:27:57 +0000

    Updated auth table for testing

--- backend/database/test.py
+++ backend/database/test.py
@@ -1,11 +1,13 @@
 import db
 import sqlite3
+import admin
 
 
 #This is where I can test the methods
 def main():
 
-    
+    '''
+    #Creates the table
     connection = sqlite3.connect('/workspaces/Pirate-Pantry-Inventory-Tracking/backend/database/db_test.db')
     cursor = connection.cursor()
 
@@ -16,35 +18,37 @@ def main():
         cursor.executescript(schema_script)
         connection.commit()
     except sqlite3.Error as e:
-        print("Oh no!")
+        print("Oh no!")'''
     
     #finally:
     
     #cursor.execute('SELECT * FROM products')
     #print(cursor.fetchall())
-    db.add_item('corn', 'HEB', 3835982, 4, 'None', ['VEGETABLES', 'canned'])
-    db.add_item('peas', 'green giant', 324, 2, 'None', ['VEGETABLES', 'canned'])
-    db.add_item('carrots', 'HEB', 9239, 1, 'None', ['VEGETABLES', 'canned'])
-    db.add_item('olives', 'everyday olives', 345345, 5, 'None', ['VEGETABLES', 'canned'])
-    db.add_item('tomato paste', 'HEB', 4329, 7, 'None', ['VEGETABLES'])
-    db.add_item('diced tomatoes', 'HEB', 49104, 6, 'None', ['VEGETABLES'])
-    db.add_item('roasted tomatoes', 'HEB', 68234, 2, 'None', ['VEGETABLES'])
-    db.add_item('cheerios', 'gereral mills', 48328, 8, 'None', ['cereal'])
-    db.add_item('frosted flakes', 'kellogs', 43453, 1, 'None', ['cereal'])
-    db.add_item('resse\'s puffs', 'general mills', 43824, 3, 'None', ['cereal', 'contains nuts'])
-    db.add_item('deodorant', 'dove', 3994294, 10, 'None', ['toiletries'])
-    db.add_item('deodorant', 'secret', 548239, 5, 'None', ['toiletries'])
-    db.add_item('deodorant', 'under armor', 5729495, 2, 'None', ['toiletries'])
-    db.add_item('corn', 'green giant', 95827, 14, 'None', ['vegetables', 'canned'])
-    db.add_item('pasta', 'HEB', 47283, 5, 'None', ['pasta', 'nut free'])
-    db.add_item('pasta', 'walmart', 1288901, 6, 'None', ['pasta', 'nut free'])
-    db.add_item('pasta', 'HEB', 390438, 8, 'None', ['pasta', 'gluten free'])
-    db.add_item('oatmeal', 'kellogs', 5743843, 2, 'None')
-    db.add_item('oatmeal', 'HEB', 1117362, 15, 'None')
-    db.add_item('liquid soap', 'dove', 3331, 5, 'None', ['soap', 'toiletries'])
-    db.add_item('hand soap', 'soft soap', 4888, 5, 'None', ['soap', 'toiletries'])
+    #db.add_item('corn', 'HEB', 3835982, 4, 'None', ['VEGETABLES', 'canned'])
+    #db.add_item('peas', 'green giant', 324, 2, 'None', ['VEGETABLES', 'canned'])
+    #db.add_item('carrots', 'HEB', 9239, 1, 'None', ['VEGETABLES', 'canned'])
+    #db.add_item('olives', 'everyday olives', 345345, 5, 'None', ['VEGETABLES', 'canned'])
+    #db.add_item('tomato paste', 'HEB', 4329, 7, 'None', ['VEGETABLES'])
+    #db.add_item('diced tomatoes', 'HEB', 49104, 6, 'None', ['VEGETABLES'])
+    #db.add_item('roasted tomatoes', 'HEB', 68234, 2, 'None', ['VEGETABLES'])
+    #db.add_item('cheerios', 'gereral mills', 48328, 8, 'None', ['cereal'])
+    #db.add_item('frosted flakes', 'kellogs', 43453, 1, 'None', ['cereal'])
+    #db.add_item('resse\'s puffs', 'general mills', 43824, 3, 'None', ['cereal', 'contains nuts'])
+    #db.add_item('deodorant', 'dove', 3994294, 10, 'None', ['toiletries'])
+    #db.add_item('deodorant', 'secret', 548239, 5, 'None', ['toiletries'])
+    #db.add_item('deodorant', 'under armor', 5729495, 2, 'None', ['toiletries'])
+    #db.add_item('corn', 'green giant', 95827, 14, 'None', ['vegetables', 'canned'])
+    #db.add_item('pasta', 'HEB', 47283, 5, 'None', ['pasta', 'nut free'])
+    #db.add_item('pasta', 'walmart', 1288901, 6, 'None', ['pasta', 'nut free'])
+    #db.add_item('pasta', 'HEB', 390438, 8, 'None', ['pasta', 'gluten free'])
+    #db.add_item('oatmeal', 'kellogs', 5743843, 2, 'None')
+    #db.add_item('oatmeal', 'HEB', 1117362, 15, 'None')
+    #db.add_item('liquid soap', 'dove', 3331, 5, 'None', ['soap', 'toiletries'])
+    #db.add_item('hand soap', 'soft soap', 4888, 5, 'None', ['soap', 'toiletries'])
     
+    print(admin.view_all())
     print(db.view_table())
+
 
 
 
