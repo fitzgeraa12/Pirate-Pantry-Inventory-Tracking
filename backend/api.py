4d6551116044c81a9b9881a17e2f7b08f8779376
amytran544 <156956580+amytran544@users.noreply.github.com>
2026-04-22 12:18:50 +0000

    Fix api.py to update checkout_times

--- backend/api.py
+++ backend/api.py
@@ -785,6 +785,7 @@ def define_routes(app: Flask, db: Database):
                     name=existing.name,
                     brand=existing.brand,
                     num_checked_out=product.amount
+                    checkout_time=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 )
 
             return jsonify({'quantities': updated_products}), 200
