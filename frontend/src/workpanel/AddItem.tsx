0293ebef1b7abe3765479ac44f89b195e8cee681
ACadeDoe <156956234+ACadeDoe@users.noreply.github.com>
2026-04-28 16:22:21 +0000

    Added revert button to admin view: attempt 5

--- frontend/src/workpanel/AddItem.tsx
+++ frontend/src/workpanel/AddItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./AddItem.css";
 import { API, type Product } from "../API";
 import { Spinner } from "../misc/misc";
